8d3776e89836d56d733a66f6fbfb04a37f257871
bahmed721 <s.bilal.ahmed927@gmail.com>
2026-09-13 13:31:59 -0600

    docs: initial project structure added

---
+++ Package.swift
@@ -0,0 +1,14 @@
+import PackageDescription
+
+let package = Package(
+    name: "macVision",
+    platforms: [
+        .macOS(.v14)
+    ],
+    products: [
+        .executable(name: "macVision", targets: ["macVision"])
+    ],
+    targets: [
+        .executableTarget(name: "macVision")
+    ]
+)
